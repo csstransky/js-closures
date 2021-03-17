b5e0597d1a8fca4c563f35ec2ab36e5f14a9cd4f
csstransky <csstransky@gmail.com>
2021-03-17 15:56:31 -0400

    Chapter 1 Lesson 4

--- Exercise Files/Ch01/01_04/start/js/script.js
+++ Exercise Files/Ch01/01_04/start/js/script.js
@@ -2,16 +2,17 @@
 
 function updateClicks() {
   let clicks = {};
-  let button;
-  function reportClicks(menu) {
-    button = menu.id;
-    clicks[button] = clicks[button] + 1 || 1;
-    console.log(button, clicks);
+  console.log("Up High ^^^");
+  function reportClicks(item) {
+    console.log("Down Low vvv");
+    clicks[item] = clicks[item] + 1 || 1;
+    console.log(item, clicks);
   }
   return reportClicks;
 }
 
-const report = updateClicks();
+const reportActivities = updateClicks();
+const reportProducts = updateClicks();
 
 const activities = {
   teamIn: ['basketball','hockey','volleyball'],
@@ -107,6 +108,12 @@ document.querySelector('.forecast-button').addEventListener('click', function(e)
 document.querySelectorAll('.options div').forEach(function(el) {
   el.addEventListener('click', function(event) {
     updateActivityList(event);
-    report(event.target);
+    reportActivities(event.target.id);
+  }, false);
+});
+
+document.querySelectorAll('.product-image').forEach(function(el){
+  el.addEventListener('mouseenter', function(event){
+    reportProducts(event.target.nextElementSibling.textContent)
   }, false);
 });
